244e601e089a1b15972839c67073ae8d2603fe56
lenghort ly <hortzz25@gmail.com>
2024-09-13 15:37:02 +0700

    drawing star

--- day4turtle.py
+++ day4turtle.py
@@ -3,6 +3,8 @@ turtle.speed(1)
 lenght = 300
 degrees = 144
 
+turtle.color("red")
+turtle.begin_fill()
 turtle.forward(lenght)
 turtle.right(degrees)
 turtle.forward(lenght)
@@ -12,6 +14,6 @@ turtle.right(degrees)
 turtle.forward(lenght)
 turtle.right(degrees)
 turtle.forward(lenght)
-
+turtle.end_fill()
 
 turtle.mainloop()
