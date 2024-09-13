ab59733333b04a84abf7aae3565d6dc79f85090c
lenghort ly <hortzz25@gmail.com>
2024-09-13 15:34:54 +0700

    input testing

--- Day3.py
+++ Day3.py
@@ -1,1 +1,6 @@
-name_input = 
+name_input = input("input your name")
+product = int(input("PRODUCT"))
+tax = int(input("TAX"))
+total = product + tax
+print("hello, wellcome", name_input)
+print("Total", total)
