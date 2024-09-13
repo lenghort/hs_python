c49e8183088c5828a812883db198470b053d3bb2
lenghort ly <hortzz25@gmail.com>
2024-09-13 15:15:01 +0700

    turtle homework

---
+++ day4turtle.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.speed(1)
+lenght = 300
+degrees = 144
+
+turtle.forward(lenght)
+turtle.right(degrees)
+turtle.forward(lenght)
+turtle.right(degrees)
+turtle.forward(lenght)
+turtle.right(degrees)
+turtle.forward(lenght)
+turtle.right(degrees)
+turtle.forward(lenght)
+
+
+turtle.mainloop()
